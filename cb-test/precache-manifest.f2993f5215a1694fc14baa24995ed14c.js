bbe1c815018e83d62a581a79256b0e3c1cade75a
Nikita <slevin070@gmail.com>
2019-08-08 00:28:06 +0300

    CbTest have added.

---
+++ cb-test/precache-manifest.f2993f5215a1694fc14baa24995ed14c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322beb38e066a31376bb794795d6b8c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59480c43aeba756b456c",
+    "url": "/static/css/main.85713c83.chunk.css"
+  },
+  {
+    "revision": "1ff20e332461771659ed",
+    "url": "/static/js/2.fb726950.chunk.js"
+  },
+  {
+    "revision": "59480c43aeba756b456c",
+    "url": "/static/js/main.5a778032.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
